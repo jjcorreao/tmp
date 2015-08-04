fabdaa4b4f68f050bd85d4346b8a80fcee621fee
Joaquin Correa <JoaquinCorrea@lbl.gov>
2015-08-03 21:23:38 -0700

    multiple changes to nersc branch

--- AR/ar-cnn_hyperopt.py
+++ AR/ar-cnn_hyperopt.py
@@ -8,7 +8,7 @@ from neon.transforms import RectLin, Logistic, CrossEntropy
 from neon.experiments import FitPredictErrorExperiment
 from neon.params import val_init
 import os
-from model import AR
+from AR.model import AR
 
 import numpy as np
 
@@ -17,7 +17,11 @@ import logging
 logging.basicConfig(level=20)
 logger = logging.getLogger()
 
-def model_gen():
+def model_gen(nofm_layer1, fshape_layer1, learning_rate_layer1, coef_layer1,
+              fshape_layer2, stride_layer2, nofm_layer3, fshape_layer3,
+              learning_rate_layer3, coef_layer3, fshape_layer4, stride_layer4,
+              learning_rate_layer5, coef_layer5, learning_rate_output, coef_output,
+              num_epochs, batch_size):
     layers = []
 
     layers.append(DataLayer(name = 'd0',
@@ -39,11 +43,11 @@ def model_gen():
 
     layers.append(ConvLayer(
         name = 'layer1',
-        nofm = 16,
-        fshape = [5,5],
+        nofm = nofm_layer1,
+        fshape = [fshape_layer1,fshape_layer1],
         weight_init = val_init.UniformValGen(low=-0.1,high=0.1),
-        lrule_init={'lr_params': {'learning_rate': 0.01,
-                'momentum_params': {'coef': 0.9, 'type': 'constant'}},
+        lrule_init={'lr_params': {'learning_rate': learning_rate_layer1,
+                'momentum_params': {'coef': coef_layer1, 'type': 'constant'}},
                 'type': 'gradient_descent_momentum'}
     ))
 
@@ -51,32 +55,32 @@ def model_gen():
     layers.append(PoolingLayer(
         name='layer2',
         op = 'max',
-        fshape = [2,2],
-        stride = 2
+        fshape = [fshape_layer2,fshape_layer2],
+        stride = stride_layer2
     ))
 
     layers.append(ConvLayer(
         name = 'layer3',
-        nofm = 32,
-        fshape = [5,5],
+        nofm = nofm_layer3,
+        fshape = [fshape_layer3,fshape_layer3],
         weight_init = val_init.UniformValGen(low=-0.1,high=0.1),
-        lrule_init={'lr_params': {'learning_rate': 0.01,
-                'momentum_params': {'coef': 0.9, 'type': 'constant'}},
+        lrule_init={'lr_params': {'learning_rate': learning_rate_layer3,
+                'momentum_params': {'coef': coef_layer3, 'type': 'constant'}},
                 'type': 'gradient_descent_momentum'}
     ))
 
     layers.append(PoolingLayer(
         name='layer4',
         op = 'max',
-        fshape = [2,2],
-        stride = 2
+        fshape = [fshape_layer4,fshape_layer4],
+        stride = stride_layer4
     ))
 
     layers.append(FCLayer(
             name = 'layer5',
             nout=500,
-            lrule_init={'lr_params': {'learning_rate': 0.01,
-                'momentum_params': {'coef': 0.9, 'type': 'constant'}},
+            lrule_init={'lr_params': {'learning_rate': learning_rate_layer5,
+                'momentum_params': {'coef': coef_layer5, 'type': 'constant'}},
                 'type': 'gradient_descent_momentum'},
             weight_init=val_init.UniformValGen(low=-0.1,high=0.1),
             activation=RectLin()
@@ -86,8 +90,8 @@ def model_gen():
     layers.append(FCLayer(
             name = 'output',
             nout = 2,
-            lrule_init={'lr_params': {'learning_rate': 0.01,
-                'momentum_params': {'coef': 0.9, 'type': 'constant'}},
+            lrule_init={'lr_params': {'learning_rate': learning_rate_output,
+                'momentum_params': {'coef': coef_output, 'type': 'constant'}},
                 'type': 'gradient_descent_momentum'},
             weight_init=val_init.UniformValGen(low=-0.1,high=0.1),
             activation = Logistic()
@@ -100,24 +104,42 @@ def model_gen():
             cost = CrossEntropy()
         )
     )
-    model = MLP(num_epochs=2, batch_size=200, layers=layers)
+    model = MLP(num_epochs=num_epochs, batch_size=batch_size, layers=layers)
 
     return model
 
 
-def hyperopt(lrate, lrule_coef, num_epochs, batch_size):
+def hyperopt(nofm_layer1, fshape_layer1, learning_rate_layer1, coef_layer1,
+           fshape_layer2, stride_layer2, nofm_layer3, fshape_layer3,
+           learning_rate_layer3, coef_layer3, fshape_layer4, stride_layer4,
+            learning_rate_layer5, coef_layer5, learning_rate_output, coef_output,
+           num_epochs, batch_size):
+
+    # basepath = "/Users/DOE6903584/NERSC/mantissa-new/AR/data"
+    # repo_path = os.path.join(basepath, "/results/")
+    # fland = os.path.join(basepath, "landmask_imgs.pkl")
+    # far = os.path.join(basepath, "atmosphericriver_TMQ.h5")
 
     basepath = "/project/projectdirs/nervana/berghain/data"
+    # basepath = "/Users/DOE6903584/NERSC/mantissa-new/AR/data"
+    repo_path = os.path.join(basepath, "/results/")
     fland = os.path.join(basepath, "landmask_imgs.pkl")
     far = os.path.join(basepath, "atmosphericriver_TMQ.h5")
 
-    dataset = AR(fland=fland, far=far)
+    dataset = AR(fland=fland, far=far, save_dir=repo_path, repo_path=repo_path)
+
     # metrics = {'train':[LogLossMean(), MisclassRate(), MSE()], 'validation':[LogLossMean(), MisclassRate(), MSE()], 'test':[]}
-    experiment = FitPredictErrorExperiment(model=model_gen(lrate, lrule_coef, num_epochs, batch_size), backend=gen_backend(),dataset=dataset)
+    experiment = FitPredictErrorExperiment(model=model_gen(nofm_layer1, fshape_layer1, learning_rate_layer1, coef_layer1,
+                                                           fshape_layer2, stride_layer2, nofm_layer3, fshape_layer3,
+                                                           learning_rate_layer3, coef_layer3, fshape_layer4, stride_layer4,
+                                                            learning_rate_layer5, coef_layer5, learning_rate_output, coef_output,
+                                                           num_epochs, batch_size), backend=gen_backend(),dataset=dataset,
+                                                            predictions = ['train', 'test'])
 
     metrics = experiment.run()
 
-    result = float(metrics['test']['MisclassPercentage_TOP_1'])
+    result = float(metrics['test']['MisclassPercentage_TOP_1'] / 100)
+
 
     # # Experiment result is dict: result[metric_set][metric_name]
     # result = experiment.run()
@@ -133,5 +155,11 @@ def hyperopt(lrate, lrule_coef, num_epochs, batch_size):
 def main(job_id, params):
 
     print 'Anything printed here will end up in the output directory for job #%d' % job_id
+
     print params
-    return hyperopt(params['lrate'], params['lrule_coef'], params['num_epochs'], params['batch_size'])
+
+    return hyperopt(params['nofm_layer1'], params['fshape_layer1'], params['learning_rate_layer1'], params['coef_layer1'],
+                params['fshape_layer2'], params['stride_layer2'], params['nofm_layer3'], params['fshape_layer3'],
+                params['learning_rate_layer3'], params['coef_layer3'], params['fshape_layer4'], params['stride_layer4'],
+                params['learning_rate_layer5'], params['coef_layer5'], params['learning_rate_output'], params['coef_output'],
+                params['num_epochs'], params['batch_size'])
